5293bd9628870519fb4f667962ccdeec0ecb837c
Siddharth Ashok <siddharthashokg@gmail.com>
2020-11-19 12:50:58 +0530

    test

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<html>
-<?php
-/* This will give an error. Note the output
- * above, which is before the header() call */
-header('Location: https://ispirt.in/volunteering-at-ispirt/');
-exit;
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "test";
+    /* This will give an error. Note the output
+     * above, which is before the header() call */
+    // header('Location: https://ispirt.in/volunteering-at-ispirt/');
+    // exit;
+    ?>
+
+  </body>
+</html>
