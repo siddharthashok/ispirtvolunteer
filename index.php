fa32e49cb461f6e1f6e37057f7083e34f86cace3
Siddharth Ashok <siddharthashokg@gmail.com>
2020-11-19 12:54:48 +0530

    js redirect

--- index.php
+++ index.php
@@ -2,16 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <script type="text/javascript">
+      window.location.replace("https://ispirt.in/volunteering-at-ispirt/");
+    </script>
   </head>
   <body>
-    <?php
-    echo "test";
-    /* This will give an error. Note the output
-     * above, which is before the header() call */
-    // header('Location: https://ispirt.in/volunteering-at-ispirt/');
-    // exit;
-    ?>
+
 
   </body>
 </html>
